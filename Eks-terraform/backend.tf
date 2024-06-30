ef7677f9234ec74c13941e3ac3c6e08513a0f21c
terencetatefua <95458889+terencetatefua@users.noreply.github.com>
2024-06-30 11:55:40 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tristy-app" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
 }
