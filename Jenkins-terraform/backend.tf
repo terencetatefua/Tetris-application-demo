0842945fcbaefc44e7c99e9be7e368d62ffaa61a
terencetatefua <terencetatefua@gmail.com>
2024-06-30 10:40:13 -0500

    updated region

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "tristy-app" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-2"
   }
